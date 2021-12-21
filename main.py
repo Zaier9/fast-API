5d5beb60db31444093c5d04cc5040e1b9402fd82
Zaier <veraalber989@gmail.com>
2021-12-21 15:07:39 -0300

    Validaciones: Path Parameters

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #Python
 from typing import Optional
-from fastapi.param_functions import Query
+from fastapi.param_functions import Path, Query
 
 #Pydantic
 from pydantic import BaseModel
@@ -8,7 +8,7 @@ from pydantic import BaseModel
 # FastAPI
 from fastapi import FastAPI
 from fastapi import Body
-from fastapi import Query
+from fastapi import Query, Path
 
 app = FastAPI()
 
@@ -37,7 +37,33 @@ def create_person(person: Person = Body(...)):
 
 @app.get("/person/detail")
 def show_person(
-    name: Optional[str] = Query(None, min_length=1, max_length=50),
-    age: Optional [int] = Query(None, ge=1, le=100)
+    name: Optional[str] = Query(
+        None, 
+        min_length=1, 
+        max_length=50,
+        title="Person Name",
+        description="This is the person name. It's between 1 and 50 characters"
+        ),
+    age: Optional [int] = Query(
+        None,
+        ge=1, 
+        le=100,
+        title="Person Age",
+        description="This is the person age. It's required"
+        )
 ):
     return {name: age}
+
+
+# Validaciones: Path Parameters
+
+@app.get("/person/detail/{person_id}")
+def show_person(
+    person_id: int = Path(
+        ..., 
+        gt=0,
+        title="Person id",
+        description="This is the personal identification"
+        )
+):
+    return{person_id: "It exist!!!"}
