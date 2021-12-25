057d64130985244ef4187df9b7a4b7832a672d99
Zaier <veraalber989@gmail.com>
2021-12-25 20:41:29 -0300

    Mas ejemplos

--- main.py
+++ main.py
@@ -83,14 +83,16 @@ def show_person(
         min_length=1, 
         max_length=50,
         title="Person Name",
-        description="This is the person name. It's between 1 and 50 characters"
+        description="This is the person name. It's between 1 and 50 characters",
+        example="Rocio"
         ),
     age: Optional [int] = Query(
         None,
         ge=1, 
         le=100,
         title="Person Age",
-        description="This is the person age. It's required"
+        description="This is the person age. It's required",
+        example=25
         )
 ):
     return {name: age}
